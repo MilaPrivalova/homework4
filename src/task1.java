5f42d28e09dcbd4e7fc79d12c6585f1b703cdb2d
Mila <milaprivalova1@gmail.com>
2022-12-08 13:06:42 +0300

    1

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,46 @@
+import java.util.*;
+
 public class task1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите минимальное число : ");
+        int min = input.nextInt();
+        System.out.println("введите максимальное число : ");
+        int max = input.nextInt();
+        System.out.println("Введите размер листа: ");
+        int s = input.nextInt();
+        input.close();
+
+        LinkedList<Integer> arr = getRandomLinkedList(s, min, max);
+        System.out.println(arr);
+        reverseLinkedList(arr);
+        System.out.println(arr);
+    }
+
+    static int getRandomNumberInt(int minimum, int maximum) {
+        Random random = new Random();
+        return random.nextInt(minimum, maximum);
+    }
+
+    static LinkedList<Integer> getRandomLinkedList(int size, int minimum, int maximum) {
+        LinkedList<Integer> array = new LinkedList<Integer>();
+        for (int i = 0; i < size; i++) {
+            array.add(getRandomNumberInt(minimum, maximum));
+        }
+        return array;
+    }
+
+    static List<Integer> reverseLinkedList(LinkedList<Integer> array) {
+        List<Integer> temp = new LinkedList<Integer>(array);
+        int j = 0;
+        for (int i = temp.size()-1; i >= 0; i--) {
+            array.set(j, temp.get(i));
+            j++;
+        }
+        return array;
+    }
+    static List<Integer> reverseLinkedList2(LinkedList<Integer> array) {
+        Collections.reverse(array);
+        return array;
+    }
 }
